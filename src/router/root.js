363de42eaa9e140c9c163ce15e4e525f21172f9d
harden <harden.lee@companywe.co.kr>
2024-04-16 14:56:51 +0900

    상품 화면 Router 설정

--- src/router/root.js
+++ src/router/root.js
@@ -1,11 +1,13 @@
 import { Suspense, lazy } from 'react'
 import { createBrowserRouter } from 'react-router-dom'
 import todoRouter from './todoRouter'
+import productRouter from './productsRouter'
 
 const Loading = <div className={'bg-red-700'}>Loading...</div>
 const Main = lazy(() => import('../pages/MainPage'))
 const About = lazy(() => import('../pages/AboutPage'))
 const TodoIndex = lazy(() => import('../pages/todo/IndexPage'))
+const ProductsIndex = lazy(() => import('../pages/product/IndexPage'))
 
 const root = createBrowserRouter([
   {
@@ -33,6 +35,11 @@ const root = createBrowserRouter([
     ),
     children: todoRouter(),
   },
+  {
+    path: 'products',
+    element: <Suspense fallback={Loading}><ProductsIndex/></Suspense>,
+    children: productRouter(),
+  }
 ])
 
 export default root
